fca6daee1468887b9da62a7bad485844e45a0c9c
yezhi <yezhi780625@gmail.com>
2018-04-12 17:32:50 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# chess
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render } from "react-dom";
+import Board from "./components/Board";
+import "./index.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      knightPosition: [0, 0]
+    };
+    this.updatePosition = this.updatePosition.bind(this);
+  }
+  canMoveKnight(toX, toY) {
+    const [x, y] = this.state.knightPosition;
+    const dx = toX - x;
+    const dy = toY - y;
+
+    return (
+      (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+      (Math.abs(dx) === 1 && Math.abs(dy) === 2)
+    );
+  }
+  moveKnight(toX, toY) {
+    const knightPosition = [toX, toY];
+    this.setState({ knightPosition });
+  }
+  updatePosition(nextPosition) {
+    const [toX, toY] = nextPosition;
+    if (this.canMoveKnight(toX, toY)) {
+      this.moveKnight(toX, toY);
+    }
+  }
+  render() {
+    return (
+      <Board
+        updatePosition={this.updatePosition}
+        knightPosition={this.state.knightPosition}
+      />
+    );
+  }
+}
+render(<App />, document.getElementById("root"));
